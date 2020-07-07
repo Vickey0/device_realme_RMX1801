bf8ce7daedbb832ebecb9e7931b6dc5ce36170a6
Hlcpereira <hlcpereira@pixelexperience.org>
2020-07-07 14:28:15 +0200

    RMX1801: Nuke IO Prefetcher

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -124,7 +124,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4
 
