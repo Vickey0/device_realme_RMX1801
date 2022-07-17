6d6714ec142cec1f8669fc4e07eeaa9249b1ff05
Krish-Na12 <109458465+Krish-Na12@users.noreply.github.com>
2022-07-17 15:29:08 +0530

    Update stag_RMX1801.mk

--- stag_RMX1801.mk
+++ stag_RMX1801.mk
@@ -18,23 +18,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Arrow stuff
-$(call inherit-product, vendor/stag/main.mk)
+# Inherit some common Blaze stuff
+$(call inherit-product, vendor/blaze/common_full_phone.mk)
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
-
+TARGET_FACE_UNLOCK_SUPPORTED := true
 # Inherit from Realme RMX1801
 $(call inherit-product, device/realme/RMX1801/device.mk)
 
 # Set Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-PRODUCT_NAME := stag_RMX1801
+PRODUCT_NAME := blaze_RMX1801
 PRODUCT_DEVICE := RMX1801
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := oppo
 PRODUCT_MODEL := Realme 2 Pro
 PRODUCT_ARCH := arm64
-DEVICE_MAINTAINER := Baibhab
+#DEVICE_MAINTAINER := Baibhab
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 TARGET_VENDOR_PRODUCT_NAME := RMX1801
