7db6a08d6a55624582de5d1894ddfacc477611f0
Vickey0 <109611665+Vickey0@users.noreply.github.com>
2022-07-19 21:00:11 +0530

    Update and rename _RMX1801.mk to syberia_RMX1801.mk

--- syberia_RMX1801.mk
+++ syberia_RMX1801.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Blaze stuff
-$(call inherit-product, vendor/blaze/common_full_phone.mk)
+$(call inherit-product, vendor/syberia/common.mk)
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USE_PIXEL_CHARGER := true
