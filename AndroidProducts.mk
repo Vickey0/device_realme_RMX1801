f5877b948d31d2239f0e93f1da03ed3c65afe8aa
Vickey0 <109611665+Vickey0@users.noreply.github.com>
2022-07-21 00:02:05 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/syberia_RMX1801.mk
+    $(LOCAL_DIR)/corvus_RMX1801.mk
 
 COMMON_LUNCH_CHOICES := \
-    syberia_RMX1801-user \
-    syberia_RMX1801-userdebug \
-    syberia_RMX1801-eng
+    corvus_RMX1801-user \
+    corvus_RMX1801-userdebug \
+    corvus_RMX1801-eng
